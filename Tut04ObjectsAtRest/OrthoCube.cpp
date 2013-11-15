993e5a1520c4cb7665e2c4b5f2b7f62589c43df6
Jarod Liu <liuyuanzhi@gmail.com>
2013-11-15 17:01:39 +0800

    Chapter 4 - Objects at Rest

---
+++ Tut04ObjectsAtRest/OrthoCube.cpp
@@ -0,0 +1,186 @@
+
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <glload/gl_3_3.h>
+#include <GLFW/glfw3.h>
+#include "../framework/framework.h"
+
+#define ARRAY_COUNT( array ) (sizeof( array ) / (sizeof( array[0] ) * (sizeof( array ) != sizeof(void*) || sizeof( array[0] ) <= sizeof(void*))))
+
+GLuint theProgram;
+
+GLuint offsetUniform;
+
+void InitializeProgram() {
+	std::vector<GLuint> shaderList;
+
+	shaderList.push_back(Framework::LoadShader(GL_VERTEX_SHADER, "OrthoWithOffset.vert"));
+	shaderList.push_back(Framework::LoadShader(GL_FRAGMENT_SHADER, "StandardColors.frag"));
+
+	theProgram = Framework::CreateProgram(shaderList);
+
+	offsetUniform = glGetUniformLocation(theProgram, "offset");
+}
+
+const float vertexData[] = {
+	 0.25f,  0.25f, 0.75f, 1.0f,
+	 0.25f, -0.25f, 0.75f, 1.0f,
+	-0.25f,  0.25f, 0.75f, 1.0f,
+
+	 0.25f, -0.25f, 0.75f, 1.0f,
+	-0.25f, -0.25f, 0.75f, 1.0f,
+	-0.25f,  0.25f, 0.75f, 1.0f,
+
+	 0.25f,  0.25f, -0.75f, 1.0f,
+	-0.25f,  0.25f, -0.75f, 1.0f,
+	 0.25f, -0.25f, -0.75f, 1.0f,
+
+	 0.25f, -0.25f, -0.75f, 1.0f,
+	-0.25f,  0.25f, -0.75f, 1.0f,
+	-0.25f, -0.25f, -0.75f, 1.0f,
+
+	-0.25f,  0.25f,  0.75f, 1.0f,
+	-0.25f, -0.25f,  0.75f, 1.0f,
+	-0.25f, -0.25f, -0.75f, 1.0f,
+
+	-0.25f,  0.25f,  0.75f, 1.0f,
+	-0.25f, -0.25f, -0.75f, 1.0f,
+	-0.25f,  0.25f, -0.75f, 1.0f,
+
+	 0.25f,  0.25f,  0.75f, 1.0f,
+	 0.25f, -0.25f, -0.75f, 1.0f,
+	 0.25f, -0.25f,  0.75f, 1.0f,
+
+	 0.25f,  0.25f,  0.75f, 1.0f,
+	 0.25f,  0.25f, -0.75f, 1.0f,
+	 0.25f, -0.25f, -0.75f, 1.0f,
+
+	 0.25f,  0.25f, -0.75f, 1.0f,
+	 0.25f,  0.25f,  0.75f, 1.0f,
+	-0.25f,  0.25f,  0.75f, 1.0f,
+
+	 0.25f,  0.25f, -0.75f, 1.0f,
+	-0.25f,  0.25f,  0.75f, 1.0f,
+	-0.25f,  0.25f, -0.75f, 1.0f,
+
+	 0.25f, -0.25f, -0.75f, 1.0f,
+	-0.25f, -0.25f,  0.75f, 1.0f,
+	 0.25f, -0.25f,  0.75f, 1.0f,
+
+	 0.25f, -0.25f, -0.75f, 1.0f,
+	-0.25f, -0.25f, -0.75f, 1.0f,
+	-0.25f, -0.25f,  0.75f, 1.0f,
+
+
+
+
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+	0.8f, 0.8f, 0.8f, 1.0f,
+
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.0f, 1.0f,
+
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+
+};
+
+GLuint vertexBufferObject;
+GLuint vao;
+
+
+void InitializeVertexBuffer() {
+	glGenBuffers(1, &vertexBufferObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void init() {
+	InitializeProgram();
+	InitializeVertexBuffer();
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+}
+
+void display() {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(theProgram);
+
+	glUniform2f(offsetUniform, 0.5f, 0.25f);
+
+	size_t colorData = sizeof(vertexData) / 2;
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)colorData);
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glUseProgram(0);
+}
+
+void reshape (GLFWwindow* window, int w, int h) {
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+}
+
+void keyboard(GLFWwindow *window, int key, int scancode, int action, int mods) {
+	switch (key) {
+	  case GLFW_KEY_ESCAPE:
+		glfwSetWindowShouldClose(window, 1);
+		return;
+	}
+}
